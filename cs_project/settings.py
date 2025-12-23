439103ad6fa445e43c443371d5a8afdadbd6794a
rockmaya <153244662+rockmaya@users.noreply.github.com>
2025-12-23 14:02:56 +0600

    5

--- cs_project/settings.py
+++ cs_project/settings.py
@@ -122,12 +122,16 @@ import dj_database_url
 #     )
 # }
 
-DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
-}
+
 DEBUG = os.environ.get('DEBUG') == 'True'
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 SECRET_KEY = os.environ.get('SECRET_KEY')
+
+DATABASES = {
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+}
+
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
--- cs_project/urls.py
+++ cs_project/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('lc/', include('lc.urls')),
+    path('', RedirectView.as_view(url='/accounts/login/', permanent=True)),
 ]
